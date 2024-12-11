d14598ecdd691486c11aae0a6caf2a4f36d91a38
yuancwwang <yuancwwang@tencent.com>
2024-12-11 10:08:08 +0800

    Initial Commit

---
+++ ios-12-11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-12-11
+//
+//  Created by Yuanchuang Wang on 2024/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-12-11/ios_12_11App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_12_11App.swift
+//  ios-12-11
+//
+//  Created by Yuanchuang Wang on 2024/12/11.
+//
+
+import SwiftUI
+
+@main
+struct ios_12_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
